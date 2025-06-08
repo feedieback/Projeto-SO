87217e1e309450af10f39caf54316235d12e25d4
Marina <marina.pina.busina@gmail.com>
2025-06-08 01:23:09 -0300

    Da uma olhada e ve se é isso.

--- Projeto-SO/Kernel.cpp
+++ Projeto-SO/Kernel.cpp
@@ -6,9 +6,10 @@ Kernel::Kernel(int quantum)
 
 void Kernel::iniciar() {
     cout << "Inicializando Sistema" << endl;
-    escalonador.adicionar_processo(Processo("P1", 5));
-    escalonador.adicionar_processo(Processo("P2", 8));
-    escalonador.adicionar_processo(Processo("P3", 6));
+
+    escalonador.adicionar_processo(Processo("P1", 5, &fs));
+    escalonador.adicionar_processo(Processo("P2", 8, &fs));
+    escalonador.adicionar_processo(Processo("P3", 6, &fs));
     cout << "Iniciando escalonador " << endl;
     escalonador.executar();
 
--- Projeto-SO/Kernel.h
+++ Projeto-SO/Kernel.h
@@ -1,10 +1,12 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 #include "Escalonador.h"
+#include "SistemaArquivos.h"
 
 class Kernel {
 private:
     EscalonadorRoundRobin escalonador;
+    SistemaArquivos fs;
 
 public:
     Kernel(int quantum);
